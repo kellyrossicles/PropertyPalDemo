bbd0ab8d2e3b100afef85eb4564912b416233cbc
Kelly Ross <kellyrossicles@gmail.com>
2021-07-24 15:48:55 +0100

    commiting tests

---
+++ cypress/integration/property_pal.js
@@ -0,0 +1,36 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
+
+describe('Page loaded successfully', () => {
+    it('logo is present', () => {
+        cy.visit('https://propertypal.com')
+          .get('.search-logo')
+          .should('be.visible')
+    });
+});
+
+describe('Search Tests', () => {
+    it('Search by My Location', () => {
+        cy.get('.search-ctrl').click()
+          .get('.suggestions-location').click()
+          .get('button[data-params=\'{ "st": "sale" }\']').click()
+          .get('.pgheader h1').contains("Property For Sale Near My Location")
+    });
+
+    it('Search by BT6', () => {
+        cy.get('.mainnav-logo').click()
+          .get('.search-logo').should('be.visible')
+          .get('.search-row .search-ctrl #query').type("{selectAll}BT6")
+          .get('button[data-params=\'{ "st": "sale" }\']').click()
+          .get('#breadcrumbs > span:nth-child(3)').contains("BT6")
+    });
+
+    it('No Results', () => {
+        cy.get('.mainnav-logo').click()
+          .get('.search-logo').should('be.visible')
+          .get('.search-row .search-ctrl #query').type("{selectAll}BT Does Not Exist")
+          .get('button[data-params=\'{ "st": "sale" }\']').click()
+          .get('.noresults-heading').contains("Sorry, No properties found")
+    });
+});
